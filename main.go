800e55762b86e5071826e223e4136cedc5c2fd3f
Johannes Fürmann <fuermannj@gmail.com>
2024-05-03 02:05:12 +0200

    Retrieve Metadata

--- main.go
+++ main.go
@@ -7,6 +7,49 @@ import (
 	"github.com/skythen/apdu"
 )
 
+var chunk_size = 64
+
+var getMetadataApdu = apdu.Capdu{
+	Cla: 0xD0,
+	Ins: 0x01,
+	P1:  byte(chunk_size),
+	P2:  00,
+	Ne:  04,
+}
+
+var selectApplication = apdu.Capdu{
+	Cla:  0x00,
+	Ins:  0xA4,
+	P1:   04,
+	P2:   00,
+	Data: []byte{0xA0, 0x00, 0xDA, 0xDA, 0xDA, 0xDA, 0xDA},
+}
+
+func transceive(device *nfc.Device, command *apdu.Capdu) (response []byte) {
+	tx, err := command.Bytes()
+	if err != nil {
+		log.Fatal("Error assembling SelectApplication APDU", err)
+	}
+	log.Infof("tx %d bytes: %x", len(tx), tx)
+
+	rx := make([]byte, 256)
+
+	n, err := device.InitiatorTransceiveBytes(
+		tx, rx, 0,
+	)
+	if err != nil {
+		log.Fatal("Error sending bytes: ", err)
+	}
+
+	log.Infof("rx %d bytes: %x", n, rx[0:n])
+
+	return rx[0:n]
+}
+
+func getToken(device *nfc.Device, len int) (string, error) {
+
+}
+
 func main() {
 	device, err := nfc.Open("")
 	if err != nil {
@@ -37,28 +80,17 @@ func main() {
 		log.Fatal("Error Selecting Target: ", err)
 	}
 
-	selectApplication := apdu.Capdu{
-		Cla: 0x00,
-		Ins: 0xA4,
-		P1:  04,
-		P2:  00,
-		// Data: []byte{0xF0, 0x01, 0x02, 0x03, 0x04, 0x05, 0x07},
-		Data: []byte{0xA0, 0x00, 0xDA, 0xDA, 0xDA, 0xDA, 0xDA},
-	}
+	transceive(&device, &selectApplication)
 
-	tx, err := selectApplication.Bytes()
+	metaResponse := transceive(&device, &getMetadataApdu)
+
+	chunk_count := int(metaResponse[0])
+	remainder := int(metaResponse[1])
+
+	log.Infof("Token has %d mod %d chunks = %d bytes", chunk_count, remainder, chunk_count*chunk_size+remainder)
+
+	err = device.Close()
 	if err != nil {
-		log.Fatal("Error assembling SelectApplication APDU", err)
+		log.Fatal("Error Closing Device: ", err)
 	}
-
-	rx := make([]byte, 256)
-
-	n, err := device.InitiatorTransceiveBytes(
-		tx, rx, 0,
-	)
-	if err != nil {
-		log.Fatal("Error sending bytes: ", err)
-	}
-
-	log.Infof("Received %d bytes: %x", n, rx[0:n])
 }
